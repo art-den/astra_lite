dbd17b42c9f0ddda76c6d8e983d02320129d251c
art-den <denis.artyomov@gmail.com>
2025-02-23 12:31:06 +0500

    build_cross.sh builds targets into separate folder (target_cross instead of target)

--- build_cross.sh
+++ build_cross.sh
@@ -3,24 +3,25 @@
 # install cross before using of this script:
 # cargo install cross --git https://github.com/cross-rs/cross
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+set -e
 
-cargo clean
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TARGET_DIR=$SCRIPT_DIR/target_cross
 
 # arm64
 
 TARGET=aarch64-unknown-linux-gnu
-cross build --release --target $TARGET
-cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=arm64 --bin=$SCRIPT_DIR/target/$TARGET/release/astra_lite"
+cross build --release --target $TARGET --target-dir $TARGET_DIR
+cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=arm64 --bin=$TARGET_DIR/$TARGET/release/astra_lite"
 
 # armhf
 
 TARGET=armv7-unknown-linux-gnueabihf
-cross build --release --target $TARGET
-cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=armhf --bin=$SCRIPT_DIR/target/$TARGET/release/astra_lite"
+cross build --release --target $TARGET --target-dir $TARGET_DIR
+cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=armhf --bin=$TARGET_DIR/$TARGET/release/astra_lite"
 
 # amd64
 
 TARGET=x86_64-unknown-linux-gnu
-cross build --release --target $TARGET
-cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=amd64 --bin=$SCRIPT_DIR/target/$TARGET/release/astra_lite"
+cross build --release --target $TARGET --target-dir $TARGET_DIR
+cross-util run --target $TARGET -- "python3 $SCRIPT_DIR/scripts/create-deb-package.py --arch=amd64 --bin=$TARGET_DIR/$TARGET/release/astra_lite"
